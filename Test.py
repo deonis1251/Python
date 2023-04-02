cb70cb1e50e7286a1d50f4051f5fec06a0ac0a4e
Denis <denis_gulbin@mail.ru>
2023-04-02 23:59:34 +0300

    Seminar2

---
+++ Test.py
@@ -0,0 +1,12 @@
+# days = int(input("Enter days: "))
+# minCount = 0
+# maxCount = 0
+# for i in range(days):
+#     temp = int(input("Enter temperature: "))
+#     if temp >= 0:
+#         maxCount += 1
+#     else: 
+#         minCount += 1
+# print(f"Days, when plus temp {maxCount} and days, when minus temp {minCount}")
+
+
