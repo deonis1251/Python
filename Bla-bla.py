af9040d6d23b109c40fa1f92ac77fbda86fd5676
Denis <denis_gulbin@mail.ru>
2023-04-05 13:07:08 +0300

    Add PySharm

--- Bla-bla.py
+++ Bla-bla.py
@@ -10,13 +10,17 @@
 # print(f"Days, when plus temp {maxCount} and days, when minus temp {minCount}")
 
 
-list1 = [1, 4, 1, 2, 4, 7]
-num = 6
-raznitsa = [num - i for i in list1]
-print(raznitsa)
-module = [abs(i) for i in raznitsa]
-print(module)
-m = min(module)
-print(m)
-print(num - m)
-# print(num - abs(min(abs(i) for i in [num - i for i in list1])))
+# list1 = [1, 4, 1, 2, 4, 7]
+# num = 6
+# raznitsa = [num - i for i in list1]
+# print(raznitsa)
+# module = [abs(i) for i in raznitsa]
+# print(module)
+# m = min(module)
+# print(m)
+# print(num - m)
+# # print(num - abs(min(abs(i) for i in [num - i for i in list1])))
+
+
+# word = 'asd,asdasd, asd  asd, asd, ,aswrfv'
+# a = word.split(' ')
---
+++ Extra/Округление.py
@@ -0,0 +1,4 @@
+a = 45164.45261654
+
+print(format(a, '.3f'))
+print(round(a, 3))
