d9616ad34827778c3c1cd46f6d6d1f9059e169da
Denis <denis_gulbin@mail.ru>
2023-04-20 14:25:47 +0300

    HW6 Task 2

--- Homeworks/Homework6/def6.py
+++ Homeworks/Homework6/def6.py
@@ -4,4 +4,20 @@ def func(first, step, lenght):
         num = first + step * el
         print(num, end=' ') 
 
-# 
+# Массив из чисел;    принимает длину массива
+def create_int_array(length_array):
+    array = []
+    for el in range(length_array):
+        num = int(input(f"Введите {el + 1} элемент массива: "))
+        array.append(num)
+    return array
+
+# Индексы чисел в диапозоне (мин, макс)
+def min_max_in_nums(nums):
+    mini = int(input("Введите минимальное значение: "))
+    maxi = int(input("Введите максимальное значение: "))
+    result = []
+    for el in range(len(nums)):
+        if mini < nums[el] < maxi:
+            result.append(el)
+    return result
--- defs.py
+++ defs.py
@@ -59,4 +59,10 @@ def near_min(nums):
             count += 1
     return count
 
+# Прогрессия   an = a1 + (n - 1) * d
+def func(first, step, lenght):
+    for el in range(lenght):
+        num = first + step * el
+        print(num, end=' ') 
+
 # 
