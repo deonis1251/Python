77d59fef2158acd5b15547685d93b5f6785ed0f6
Denis <denis_gulbin@mail.ru>
2023-04-27 13:27:52 +0300

    Sem8 change way

--- Seminars/Seminar_8/funcs.py
+++ Seminars/Seminar_8/funcs.py
@@ -1,10 +1,10 @@
 def add_contact(contact):
-    with open('text.txt', "a") as data:
+    with open('Seminars/Seminar_8/text.txt', "a") as data:
         data.writelines(contact)
         data.write("\n")
 
 def all_contacts():
-    contact_list = open('text.txt', 'r')
+    contact_list = open('Seminars/Seminar_8/text.txt', 'r')
     num = 1
     for el in contact_list:
         print(f'{num}. {el}')
@@ -12,7 +12,7 @@ def all_contacts():
     contact_list.close()
 
 def search_countact():
-    contact_list = open('text.txt', 'r')
+    contact_list = open('Seminars/Seminar_8/text.txt', 'r')
     search = input("Введите фамилию для поиска: ").title()
     for el in contact_list:
         if search in el:
